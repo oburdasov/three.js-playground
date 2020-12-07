c03202037f180e26148fd93ce9d07c73b580abb5
oburdasov <oleg.burdasov@diabolocom.com>
2020-12-07 17:46:45 +0300

    multiple opaque cubes animation

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import {
 const scene = new Scene();
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const mouse = new Vector2();
-camera.position.z = 3;
+camera.position.z = 10;
 
 const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -20,23 +20,62 @@ document.body.appendChild(renderer.domElement);
 
 // Cube
 const geometry = new BoxGeometry();
-const material = new MeshBasicMaterial({color: 0x333333});
-const cube = new Mesh(geometry, material);
-scene.add(cube);
+
+function fillCubes() {
+  for (let i = 0; i < 20; i++) {
+    var randomColor = Math.floor(Math.random()*16777215);
+    const material = new MeshBasicMaterial({color: randomColor});
+    material.transparent = true;
+    material.opacity = 0.2;
+    const cube = new Mesh(geometry, material);
+    cube.position.x = Math.random() * 10 - 5;
+    cube.position.y = Math.random() * 10 - 5;
+    cube.position.z = Math.random() * 10;
+    cube.rotation.x = Math.random() * 2;
+    cube.rotation.y = Math.random() * 2;
+    scene.add(cube);
+  }
+}
+fillCubes();
 
 // Update
+let isContracting = true;
 function animate() {
   requestAnimationFrame(animate);
+  scene.children.forEach(cube => {
+    isContracting ?
+      contract(cube) :
+      expand(cube);
+  })
   renderer.render(scene, camera);
-
-  cube.rotation.x -= mouse.y / 10;
-  cube.rotation.y += mouse.x / 10;
 }
 animate();
 
+function contract(cube) {
+  cube.position.x = cube.position.x / 1.01;
+  cube.position.y = cube.position.y / 1.01;
+  cube.position.z = cube.position.y / 1.01;
+  cube.rotation.x = cube.rotation.x / 1.01;
+  cube.rotation.y = cube.rotation.y / 1.01;
+}
+
+function expand(cube) {
+  cube.position.x = cube.position.x * 1.01;
+  cube.position.y = cube.position.y * 1.01;
+  cube.position.z = cube.position.y * 1.01;
+  cube.rotation.x = cube.rotation.x * 1.01;
+  cube.rotation.y = cube.rotation.y * 1.01;
+}
+
 // Controls
-function onMouseMove(event) {
+window.addEventListener('mousemove', event => {
   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-}
-window.addEventListener('mousemove', onMouseMove, false);
+} , false);
+
+window.addEventListener('click', () => {
+  scene.clear();
+  fillCubes();
+}, false);
+
+setInterval(() => isContracting = !isContracting, 2000);
